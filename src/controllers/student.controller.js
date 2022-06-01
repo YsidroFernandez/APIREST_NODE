e1f10c043131faede9863196ce1b838965997781
Ysidro Fernandez <ysidrofernandez3012@gmail.com>
2022-06-01 19:00:54 -0400

    delete model

--- src/controllers/student.controller.js
+++ src/controllers/student.controller.js
@@ -5,7 +5,6 @@ const path = require("path");
 const DB_PATH = path.join(__dirname + '/../../data_json/students.json');
 
 let db = require(DB_PATH);
-const { StudentModel } = require("../models");
 
 
 async function getAllStudents(req,resp){
--- src/models/index.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    StudentModel : require("./student.model")
-}
