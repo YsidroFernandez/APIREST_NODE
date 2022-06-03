4dd0ca878c91437dec98167d8817ffb61553f2e7
Ysidro Fernandez <ysidrofernandez3012@gmail.com>
2022-06-03 14:40:02 -0400

    ADD: CORS Setting

--- src/server/index.js
+++ src/server/index.js
@@ -7,19 +7,20 @@ const api = require("../routes");
 const corsOptions = {
     origin : '*',
     methods : "GET,HEAD,PUT,PATCH,POST,DELETE, OPTIONS",
-    preflightContinue : true,
+    preflightContinue : false,
     optionsSuccessStatus : 200 ,
-    credentials : true,
+    credentials : false,
     allowedHeaders : ['X-Requested-With, Content-Type,X-Amz-Date, Authorization, X-Api-Key, Origin, Accept, Access-Control-Allow-Headers, Access-Control-Allow-Methods, Access-Control-Allow-Origin']
   }
 
 
+
 // Configurar cabeceras y cors include before other routes
 
-server.use('/api', api);
 server.use(cors(corsOptions));
-server.use(body_parser.urlencoded({extended : false }));
+server.use(body_parser.urlencoded({ extended: false }));
 server.use(body_parser.json());
 server.use(express.static(__dirname + "/../public"));
+server.use('/api', api);
 
 module.exports = server;
